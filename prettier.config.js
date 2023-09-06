b8e929b284d70c4e796799a4dd94b807382157f6
mogiel <mogielski@protonmail.com>
2023-09-06 19:55:19 +0200

    feat: config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	semi: false,
+	singleQuote: false,
+	trailingComma: "all",
+	printWidth: 100,
+	useTabs: true,
+	plugins: [
+		"prettier-plugin-tailwindcss",
+	],
+	tailwindConfig:
+		"./tailwind.config.js",
+}
