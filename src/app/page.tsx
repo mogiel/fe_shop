fdc73401e844d4b4298303ad581a12443e49291c
mogiel <mogielski@protonmail.com>
2023-09-06 20:16:27 +0200

    feat: page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,14 @@
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1>
-				Hello world
-			</h1>
-			<h1>
-				Hello world
-				asdsadsa
-			</h1>
-			<h1>
-				Hello world
-			</h1>
-			<h1>
-				Hello world
-			</h1>
-			<h1>
-				Hello world
-			</h1>
-			<h1>
-				Hello world
-			</h1>
-			<h1>
-				Hello world
-			</h1>
-			<h1>
-				Hello world
-			</h1>
+			<h1>Hello world</h1>
+			<h1>Hello world</h1>
+			<h1>Hello world</h1>
+			<h1>Hello world</h1>
+			<h1>Hello world</h1>
+			<h1>Hello world</h1>
+			<h1>Hello world</h1>
+			<h1>Hello world</h1>
 		</main>
 	)
 }
