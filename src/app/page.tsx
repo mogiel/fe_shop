b3545b4a33c9aa915887699bef1a176735991d09
mogiel <mogielski@protonmail.com>
2023-09-07 08:42:03 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center p-24">
             <h1 className={"mb-5 text-3xl"}>Sklep osiedlowy</h1>
-            <ul>
-                <li data-testid="products-list">Mąka</li>
-                <li data-testid="products-list">Cukier</li>
-                <li data-testid="products-list">Chleb</li>
-                <li data-testid="products-list">Masło</li>
+            <ul data-testid="products-list">
+                <li >Mąka</li>
+                <li>Cukier</li>
+                <li>Chleb</li>
+                <li>Masło</li>
             </ul>
         </main>
     )
