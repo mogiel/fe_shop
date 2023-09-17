dd60325e34605eb7463364144298dca965ed82f9
mogiel <mogielski@protonmail.com>
2023-09-17 20:48:55 +0200

    active link

--- src/ui/atoms/ActiveLink/ActiveLink.tsx
+++ src/ui/atoms/ActiveLink/ActiveLink.tsx
@@ -1,16 +1,17 @@
 "use client"
 
+import type { Route } from "next"
 import Link from "next/link"
 import clsx from "clsx"
 import { usePathname } from "next/navigation"
 import { type ReactNode } from "react"
 
-interface Props {
+interface Props<T extends string> {
 	children: ReactNode
-	href: string
+	href: Route<T> | URL
 }
 
-export const ActiveLink = ({ children, href }: Props) => {
+export const ActiveLink = ({ children, href }: Props<string>) => {
 	const pathname = usePathname()
 	const isActive = pathname === href
 
